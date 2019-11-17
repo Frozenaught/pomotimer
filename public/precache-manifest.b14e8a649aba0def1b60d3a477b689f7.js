b3ce6d09ae7ff1d17eaa8041399dad9633c9c43f
Frozenaught <nico.britz@outlook.com>
2019-11-17 23:33:47 +0000

    Initial Commit

---
+++ public/precache-manifest.b14e8a649aba0def1b60d3a477b689f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8191c4fd2f1436198d39bbde43403cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "203d40d1200309434c26",
+    "url": "/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "d7efb230257d9db3d50c",
+    "url": "/static/css/main.4ba2ce2e.chunk.css"
+  },
+  {
+    "revision": "203d40d1200309434c26",
+    "url": "/static/js/2.9d55c62d.chunk.js"
+  },
+  {
+    "revision": "d7efb230257d9db3d50c",
+    "url": "/static/js/main.d349b6a7.chunk.js"
+  },
+  {
+    "revision": "75ec4ecc74e149664375",
+    "url": "/static/js/runtime-main.971c9615.js"
+  }
+]);
